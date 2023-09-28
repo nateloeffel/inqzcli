27f8e5ed216419fa15088ce2922b323a606c1d36
nateloeffel <nateloeffel@inqz.net>
2023-09-28 16:43:35 -0400

    fixed checkip

--- main.go
+++ main.go
@@ -17,6 +17,29 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!")
 }
 
+func getIPAddress() string {
+	resp, err := http.Get("https://api.ipify.org?format=json")
+	if err != nil {
+		return ""
+	}
+	defer resp.Body.Close()
+
+	// Reading the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return ""
+	}
+
+	// Unmarshalling the JSON response
+	var ipResponse IPResponse
+	err = json.Unmarshal(body, &ipResponse)
+	if err != nil {
+		return ""
+	}
+
+	return ipResponse.IP
+}
+
 func main() {
 
 	args := os.Args
@@ -63,7 +86,26 @@ func main() {
 			fmt.Println("You must pass an argument with create.")
 		}
 	} else if args[1] == "ip" {
-		resp, err := http.Get("https://api.ipify.org?format=json")
+
+		fmt.Println("Your IP address is:", getIPAddress())
+	} else if args[1] == "serve" {
+		port := "8080"
+		if len(args) > 2 {
+			port = string(args[2])
+		}
+		http.HandleFunc("/", handler)
+		fmt.Println("Serving on port " + port)
+		http.ListenAndServe(":"+port, nil)
+
+	} else if args[1] == "checkip" {
+		ip := ""
+		if len(args) < 3 {
+			ip = getIPAddress()
+		} else {
+			ip = args[2]
+		}
+
+		resp, err := http.Get("http://api.ipstack.com/" + ip + "?access_key=c7ceecbdf42731fd5e0a58271ebf8b43")
 		if err != nil {
 			fmt.Println("Error:", err)
 			return
@@ -78,64 +120,19 @@ func main() {
 		}
 
 		// Unmarshalling the JSON response
-		var ipResponse IPResponse
-		err = json.Unmarshal(body, &ipResponse)
+		var data map[string]interface{}
+		err = json.Unmarshal(body, &data)
 		if err != nil {
 			fmt.Println("Error:", err)
 			return
 		}
-		// Printing the IP address
-		fmt.Println("Your IP address is:", ipResponse.IP)
-	} else if args[1] == "serve" {
-		port := "8080"
-		if len(args) > 2 {
-			port = string(args[2])
-		}
-		http.HandleFunc("/", handler)
-		fmt.Println("Serving on port " + port)
-		http.ListenAndServe(":"+port, nil)
 
-	} else if args[1] == "checkip" {
-		if len(args) > 2 {
-			ip := args[2]
-			resp, err := http.Get("http://api.ipstack.com/" + ip + "?access_key=c7ceecbdf42731fd5e0a58271ebf8b43")
-			if err != nil {
-				fmt.Println("Error:", err)
-				return
-			}
-			defer resp.Body.Close()
-
-			// Reading the response body
-			body, err := io.ReadAll(resp.Body)
-			if err != nil {
-				fmt.Println("Error:", err)
-				return
-			}
-
-			// Unmarshalling the JSON response
-			var data map[string]interface{}
-			err = json.Unmarshal(body, &data)
-			if err != nil {
-				fmt.Println("Error:", err)
-				return
-			}
-
-			// Printing the important information
-			fmt.Println("IP Address:", data["ip"])
-			fmt.Println("Country:", data["country_name"])
-			fmt.Println("Region:", data["region_name"])
-			fmt.Println("City:", data["city"])
-			fmt.Println("Latitude:", data["latitude"], "Longitude:", data["longitude"])
-			fmt.Println("Zip Code:", data["zip"])
-			connection, ok := data["connection"].(map[string]interface{})
-			if !ok {
-				fmt.Println("Error: could not parse connection information")
-				return
-			}
-			fmt.Println("ISP:", connection["isp"])
-
-		} else {
-			fmt.Println("You must pass an IP address with geolocate.")
-		}
+		// Printing the important information
+		fmt.Println("IP Address:", data["ip"])
+		fmt.Println("Country:", data["country_name"])
+		fmt.Println("Region:", data["region_name"])
+		fmt.Println("City:", data["city"])
+		fmt.Println("Latitude:", data["latitude"], "Longitude:", data["longitude"])
+		fmt.Println("Zip Code:", data["zip"])
 	}
 }
