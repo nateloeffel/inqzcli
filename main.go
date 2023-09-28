e60848cbdb6f91b7438d04dfd4093a115b8e7fb9
nateloeffel <nateloeffel@inqz.net>
2023-09-28 16:07:27 +0000

    You must pass an argument with create now

--- main.go
+++ main.go
@@ -15,35 +15,41 @@ func main() {
 	}
 
 	if args[1] == "create" {
-		if len(args) > 2 && args[2] == "discord" {
-			fmt.Println("Creating Discord Bot")
+		if len(args) > 2 {
+			if args[2] == "discord" {
+				path := "BaseDiscordBot"
+				if len(args) > 3 {
+					path = args[3]
+				}
+				fmt.Println("Creating Discord Bot")
+				cmd := exec.Command("git", "clone", "https://github.com/nateloeffel/BaseDiscordBot.git", path)
+				_, err := cmd.CombinedOutput()
+				if err != nil {
+					fmt.Println("Error:", err)
+					return
+				}
 
-			cmd := exec.Command("git", "clone", "https://github.com/nateloeffel/BaseDiscordBot.git")
-			_, err := cmd.CombinedOutput()
-			if err != nil {
-				fmt.Println("Error:", err)
-				return
-			}
+				file, err := os.OpenFile(path+"/.env", os.O_WRONLY|os.O_CREATE, 0644)
+				if err != nil {
+					// Handle the error
+					panic(err)
+				}
+				defer file.Close()
 
-			file, err := os.OpenFile("./BaseDiscordBot/.env", os.O_WRONLY|os.O_CREATE, 0644)
-			if err != nil {
-				// Handle the error
-				panic(err)
-			}
-			defer file.Close()
+				error := os.Remove(path + "/.env.example")
+				if error != nil {
+					panic(err)
+				}
 
-			error := os.Remove("./BaseDiscordBot/.env.example")
-			if error != nil {
-				panic(err)
+				// Write string to file
+				_, err = file.WriteString("GUILD_ID=\nCLIENT_ID=\nTOKEN=")
+				if err != nil {
+					// Handle the error
+					panic(err)
+				}
 			}
-
-			// Write string to file
-			_, err = file.WriteString("GUILD_ID=\nCLIENT_ID=\nTOKEN=")
-			if err != nil {
-				// Handle the error
-				panic(err)
-			}
+		} else {
+			fmt.Println("You must pass an argument with create.")
 		}
-
 	}
 }
